3601112356268a7f2382bf21a1f45aa7afbbd522
geadwiputrinkg-hash <geadwiputrinkg@gmail.com>
2026-06-26 11:10:04 +0700

    changes on dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot>
-    <h3 class="text-white text-xl">Welcome to the Dashboard</h3>
+    <h3 class="text-white text-xl">Welcome to the Dashboards</h3>
 </x-layout>
