200b5a2b2d405aa45e21461c1734982d38fcfb3a
jonathanchriss889-boop <jonathanchriss889@gmail.com>
2026-07-09 11:40:18 +0700

    menghapus fungsi lupa password

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -59,7 +59,6 @@
                     <input type="checkbox" name="remember" class="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500">
                     <span>Ingat Saya</span>
                 </label>
-                <a href="#" class="text-blue-600 hover:underline">Lupa Password?</a>
             </div>
 
             <button type="submit" 
